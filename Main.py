a3f58439dfc3df91b02f2531a4b6f370c3244c8c
Alondra Sanchez <asanch92@students.kennesaw.edu>
2024-11-18 23:07:27 -0500

    Uploading Database
    I am connecting the spreadsheet containing the bad ingredients through Panda

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import os
 
+#here i am asking for the specific database
+file_name = "ingredient makeup.xlsx"
+file_path = r'C: /Users/alondra/Downloads'
+file = os.path.join(file_path,file_name)
+
+df = pd.read_excel(file) #use pandas on it
+
+
+
+
+
+
+

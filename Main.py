7581f2910f0d04f918c12471e913891a17f4dfdc
Alondra Sanchez <asanch92@students.kennesaw.edu>
2024-11-18 23:21:58 -0500

    Summarized Version of bigger DB
    Because the actual database is bigger, The reasons will need to be added later or implemented using another program.

--- Main.py
+++ Main.py
@@ -8,6 +8,22 @@ file = os.path.join(file_path,file_name)
 
 df = pd.read_excel(file) #use pandas on it
 
+# This is a simple code that we will expand the project on. 
+pore_clogging_ingredients = {
+    "coconut oil": "Coconut oil is pore-clogging because the molecules are too big for your pore size, especially if you already struggle with keratinization.",
+    "lanolin": "Lanolin is highly comedogenic and can trap dirt and bacteria in your pores, leading to breakouts.",
+    "isopropyl myristate": "Isopropyl myristate clogs pores and exacerbates acne-prone skin.",
+    "wheat germ oil": "Wheat germ oil has a high comedogenic rating and can block pores, causing acne.",
+    "algae extract": "Algae extract can be too rich for sensitive or acne-prone skin, leading to clogged pores.",
+}
+
+
+
+
+
+
+
+
 
 
 
